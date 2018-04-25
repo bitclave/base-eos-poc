cf070b163cbad7f0681ae6a7866c0055c3b81a9c
Mark Shwartzman <markshw@gmail.com>
2018-04-24 17:24:20 -0700

    simple test using latest eos docker

---
+++ run_test.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+docker-compose up -d
+
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd wallet create
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd wallet import 5Ji8rDYiuqTAzocRPgz6EdT9szM7ootSBeVDxNm6p2rsUCRn1Yh
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd wallet import 5JmEDUXG88AnikGzov8MZVESaN1zBscYbpiqJrS2SFUedqfUL8c
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd create account eosio hello3 EOS5g1it16cQG9aXZoHdTdG8abk2bA6Mk1ojEnw7bhJWvC28a81gJ EOS5LSwDqyYt9a65ebYATJ2gqXhSxfaAi8Evpn9tz1w81tcD5N7Vy
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd set contract hello3 /mnt/contracts/hello
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd get table hello3 tester keyval
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd create account eosio user EOS5g1it16cQG9aXZoHdTdG8abk2bA6Mk1ojEnw7bhJWvC28a81gJ EOS5g1it16cQG9aXZoHdTdG8abk2bA6Mk1ojEnw7bhJWvC28a81gJ
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd create account eosio tester EOS5g1it16cQG9aXZoHdTdG8abk2bA6Mk1ojEnw7bhJWvC28a81gJ EOS5g1it16cQG9aXZoHdTdG8abk2bA6Mk1ojEnw7bhJWvC28a81gJ
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd push action hello3 addsec '["tester", "testerabcd_38a", "BBBB"]' -p tester
+docker-compose exec keosd /opt/eosio/bin/cleos -H nodeosd get table hello3 tester keyval
+
+docker-compose down
+
+
